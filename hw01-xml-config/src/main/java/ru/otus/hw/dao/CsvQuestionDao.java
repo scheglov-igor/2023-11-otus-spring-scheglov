5eada8cca50d7154019d8f3f4a48955a069adf47
Igor Scheglov <scheglov.igor@gmail.com>
2023-12-26 03:24:40 +0300

    ready

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,11 +1,19 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
@@ -18,6 +26,18 @@ public class CsvQuestionDao implements QuestionDao {
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+        Resource resource = new ClassPathResource(fileNameProvider.getTestFileName());
+
+        try {
+            File file = resource.getFile();
+            List<QuestionDto> beans = new CsvToBeanBuilder(new FileReader(file))
+                    .withType(QuestionDto.class).withSeparator(';').withSkipLines(1).build().parse();
+
+            return beans.stream().map(QuestionDto::toDomainObject).collect(Collectors.toList());
+
+        } catch (IOException e) {
+            throw new QuestionReadException("can't find questions file", e);
+        }
+
     }
 }
