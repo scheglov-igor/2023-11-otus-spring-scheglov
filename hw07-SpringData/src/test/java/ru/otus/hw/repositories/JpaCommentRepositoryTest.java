c2f5621aae4f9b4e3d795931f7cb5b60f5c873af
Igor Scheglov <scheglov.igor@gmail.com>
2024-08-22 01:36:59 +0300

    hw07 ready

---
+++ hw07-SpringData/src/test/java/ru/otus/hw/repositories/JpaCommentRepositoryTest.java
@@ -0,0 +1,155 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ru.otus.hw.models.Author;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Comment;
+import ru.otus.hw.models.Genre;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе JPA для работы с комментариями ")
+@DataJpaTest
+class JpaCommentRepositoryTest {
+
+    @Autowired
+    private CommentRepository repository;
+
+    private List<Author> dbAuthors;
+
+    private List<Genre> dbGenres;
+
+    private List<Book> dbBooks;
+    private List<Comment> commentList;
+
+    @BeforeEach
+    void setUp() {
+        dbAuthors = getDbAuthors();
+        dbGenres = getDbGenres();
+        dbBooks = getDbBooks(dbAuthors, dbGenres);
+        commentList = getDbComments();
+    }
+
+    @DisplayName("должен загружать комментарий по id")
+    @ParameterizedTest
+    @MethodSource("getDbComments")
+    void shouldReturnCorrectCommentById(Comment expectedComment) {
+        var actualComment = repository.findById(expectedComment.getId());
+        assertThat(actualComment).isPresent()
+                .get()
+                .isEqualTo(expectedComment);
+    }
+
+    @DisplayName("должен загружать список комментариев по id книги")
+    @Test
+    void shouldReturnCorrectBooksList() {
+        var actualComments = repository.findByBookId(1L);
+        var expectedComments = List.of(
+                commentList.get(0),
+                commentList.get(1));
+
+        assertThat(actualComments).containsExactlyElementsOf(expectedComments);
+        actualComments.forEach(System.out::println);
+    }
+
+
+    @DisplayName("должен сохранять новый комментарий")
+    @Test
+    void shouldSaveNewComment() {
+        var expectedComment = new Comment(4L, dbBooks.get(0), "comment_10500");
+        var returnedComment = repository.save(expectedComment);
+        assertThat(returnedComment).isNotNull()
+                .matches(comment -> comment.getId() > 0)
+                .usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(expectedComment);
+
+        assertThat(repository.findById(returnedComment.getId()))
+                .isPresent()
+                .get()
+                .isEqualTo(returnedComment);
+    }
+
+
+    @DisplayName("должен сохранять измененный комментарий")
+    @Test
+    void shouldSaveUpdatedComment() {
+        var expectedComment = new Comment(1L, dbBooks.get(0), "comment_100500");
+
+        assertThat(repository.findById(expectedComment.getId()))
+                .isPresent()
+                .get()
+                .isNotEqualTo(expectedComment);
+
+        var returnedComment = repository.save(expectedComment);
+
+        assertThat(returnedComment).isNotNull()
+                .matches(comment -> comment.getId() > 0)
+                .usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(expectedComment);
+
+        assertThat(repository.findById(returnedComment.getId()))
+                .isPresent()
+                .get()
+                .isEqualTo(returnedComment);
+    }
+
+
+    @DisplayName("должен удалять комментарий по id ")
+    @Test
+    void shouldDeleteComment() {
+        var existingComment = repository.findById(1L);
+        assertThat(existingComment).isPresent();
+        repository.delete(existingComment.get());
+        assertThat(repository.findById(1L)).isEmpty();
+    }
+
+    private static List<Author> getDbAuthors() {
+        return LongStream.range(1, 4).boxed()
+                .map(id -> new Author(id, "Author_" + id))
+                .toList();
+    }
+
+    private static List<Genre> getDbGenres() {
+        return LongStream.range(1, 7).boxed()
+                .map(id -> new Genre(id, "Genre_" + id))
+                .toList();
+    }
+
+    private static List<Book> getDbBooks(List<Author> dbAuthors, List<Genre> dbGenres) {
+        return IntStream.range(1, 4).boxed()
+                .map(id -> new Book(id.longValue(),
+                        "BookTitle_" + id,
+                        dbAuthors.get(id - 1),
+                        dbGenres.subList((id - 1) * 2, (id - 1) * 2 + 2)
+                ))
+                .toList();
+    }
+
+    private static List<Book> getDbBooks() {
+        var dbAuthors = getDbAuthors();
+        var dbGenres = getDbGenres();
+        return getDbBooks(dbAuthors, dbGenres);
+    }
+
+
+    private static List<Comment> getDbComments() {
+
+        var dbBooks = getDbBooks();
+        return List.of(
+                new Comment(1L, dbBooks.get(0), "comment_1"),
+                new Comment(2L, dbBooks.get(0), "comment_2"),
+                new Comment(3L, dbBooks.get(1), "comment_3")
+        );
+    }
+
+
+}
---
+++ hw07-SpringData/src/test/java/ru/otus/hw/services/GenreServiceImplTest.java
@@ -0,0 +1,44 @@
+package ru.otus.hw.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.converters.GenreConverter;
+import ru.otus.hw.dto.GenreDto;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Сервис для работы с жанрами")
+@DataJpaTest
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@Import({GenreServiceImpl.class, GenreConverter.class})
+class GenreServiceImplTest {
+
+    @Autowired
+    private GenreServiceImpl genreServiceImpl;
+
+    private List<GenreDto> genreDtoList;
+
+    @BeforeEach
+    void setUp() {
+        genreDtoList = StandartExpectedDtoProvider.getGenreDtoList();
+    }
+
+    @DisplayName("должен загружать список всех жанров")
+    @Test
+    void shouldReturnCorrectAllGenresList() {
+        var actualGenres = genreServiceImpl.findAll();
+        var expectedGenres = genreDtoList;
+
+        assertThat(actualGenres).containsExactlyElementsOf(expectedGenres);
+        expectedGenres.forEach(System.out::println);
+    }
+
+}
