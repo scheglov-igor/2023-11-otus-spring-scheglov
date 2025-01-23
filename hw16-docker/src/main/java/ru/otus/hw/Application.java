62b76a3c514d84dab2c976eb4fa507de26c7dff4
Igor Scheglov <scheglov.igor@gmail.com>
2025-01-23 03:38:44 +0300

    hw16-final

--- hw16-docker/src/main/java/ru/otus/hw/Application.java
+++ hw16-docker/src/main/java/ru/otus/hw/Application.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		com.sun.management.OperatingSystemMXBean os = (com.sun.management.OperatingSystemMXBean)
+				java.lang.management.ManagementFactory.getOperatingSystemMXBean();
+		System.out.println("availableProcessors:" + Runtime.getRuntime().availableProcessors());
+		System.out.println("TotalMemorySize, mb:" + os.getTotalMemorySize() / 1024 / 1024);
+		System.out.println("maxMemory, mb:" + Runtime.getRuntime().maxMemory() / 1024 / 1024);
+		System.out.println("freeMemory, mb:" + Runtime.getRuntime().freeMemory() / 1024 / 1024);
+
 		SpringApplication.run(Application.class, args);
 		System.out.printf("Чтобы перейти на страницу сайта открывай: %n%s%n",
 				"http://localhost:8080");
